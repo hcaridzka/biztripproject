42afdb317586d315330239027385c3c1350c3d57
hcaridzka <hr.aridzka@gmail.com>
2026-08-15 17:49:16 +0700

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,8 +1,9 @@
 import { createClient } from '@supabase/supabase-js';
 
-const url = import.meta.env.VITE_SUPABASE_URL;
-const anon = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-export const supabase = createClient(url, anon, {
-  auth: { persistSession: true, autoRefreshToken: true },
-});
+if (!supabaseUrl || !supabaseAnnonKey) {
+  console.error('Kredensial supabase tidak ditemukan!'); }
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
